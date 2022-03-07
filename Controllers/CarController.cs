f6ea7b810b7a80aabb5859d5ac586745892de949
Bollsjen <55839056+Bollsjen@users.noreply.github.com>
2022-03-07 13:35:32 +0100

    Initial commit

---
+++ Controllers/CarController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+using OpligatoristRESTAPI.Managers;
+
+using ObligatoriskOpgave.Models;
+
+namespace OpligatoristRESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly CarManager _manager = new CarManager();
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        public ActionResult<IEnumerable<Car>> OnGet()
+        {
+            IEnumerable<Car> list = _manager.GetAll();
+            if (list == null)
+                return NotFound();
+
+            return Ok(list);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        public ActionResult<IEnumerable<Car>> OnGet([FromQuery]string substring)
+        {
+            IEnumerable<Car> list = _manager.GetAll(substring);
+            if (list == null)
+                return NotFound();
+
+            return Ok(list);
+        }
+
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<Car>> OnGet(int id)
+        {
+            Car car = _manager.GetById(id);
+            if (car == null)
+                return NotFound();
+
+            return Ok(car);
+        }
+
+
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpPost]
+        public ActionResult<Car> Post([FromBody] Car car)
+        {
+            Car _car = _manager.CreateCar(car);
+            if (_car != null)
+                return Ok(_car);
+            else
+                return NotFound();
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpPut("{id}")]
+        public ActionResult<Car> Put([FromBody] Car car, int id)
+        {
+            Car modCar = _manager.UpdateCar(car, id);
+            if (modCar != null)
+                return Ok(modCar);
+
+            return NotFound();
+        }
+
+
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpDelete("{id}")]
+        public ActionResult<IEnumerable<Car>> OnDelete(int id)
+        {
+            Car deletedCar = _manager.DeleteCar(id);
+            if (deletedCar == null)
+                return NotFound();
+
+            return Ok(deletedCar);
+        }
+    }
+}
---
+++ Managers/CarManager.cs
@@ -0,0 +1,60 @@
+﻿using ObligatoriskOpgave.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpligatoristRESTAPI.Managers
+{
+    public class CarManager
+    {
+        private static int nextID = 1;
+        private static List<Car> Cars = new List<Car>()
+        {
+            new Car(nextID++,"Model X", 599999.95,"XD69420"),
+            new Car(nextID++,"XC90", 449999.95, "TR69421"),
+            new Car(nextID++,"Golf", 120000, "BRIAN")
+        };
+
+
+        public IEnumerable<Car> GetAll()
+        {
+            return Cars;
+        }
+
+        public IEnumerable<Car> GetAll(string substring)
+        {
+            return Cars.;
+        }
+
+        public Car GetById(int id)
+        {
+            return Cars.Find(c => c.ID == id);
+        }
+
+        public Car CreateCar(Car car)
+        {
+            car.ID = nextID++;
+            Cars.Add(car);
+            return car;
+        }
+
+        public Car UpdateCar(Car car, int id)
+        {
+            Car modCar = GetById(id);
+            modCar.Model = car.Model;
+            modCar.LicensePlate = car.LicensePlate;
+            modCar.Price = car.Price;
+            return modCar;
+        }
+
+        public Car DeleteCar(int id)
+        {
+            int index = Cars.FindIndex(c => c.ID == id);
+            Car toBeDeleted = Cars[index];
+            Cars.RemoveAt(index);
+            
+            return toBeDeleted;
+        }
+    }
+}
